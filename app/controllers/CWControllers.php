b4463fb42a46528395a4f9dc65ecb8f1d1698434
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2022-10-07 14:18:24 +0200

    added controllers for the warehouse hub and warehouse pages

--- app/controllers/CWControllers.php
+++ app/controllers/CWControllers.php
@@ -2,17 +2,55 @@
     class CWControllers extends Controller
     {
         
+        private $db;
+        
         public function __construct()
         {
+            $this->db = new Database();
+            // $this->id = $_GET['id'];
+            // $id = $_GET['id'];
+            
             //refers to the model.
             $this->cwModel = $this->model('CWModel');
+            //var_dump($this->getAll());
         }
-
-        public function index()
+        private function alert(){
+            $this->alert("Dit bestaat niet");
+            $this->view('warehouse/index', );
+            exit;
+        }
+        public function getAll()
         {
+            $this->db->query("SELECT * FROM warehouse");
+            return $this->db->resultSet();
+        }
+        public function overview($id) {
+            
+            if($id == 1 ){
+                $this->view('warehouse/1');
+                        }
+            elseif($id == 2){
+                $this->view('warehouse/2');
+            }
+            elseif($id == 3){
+                $this->view('warehouse/3');
+            }
+            else{
+                //ob_start();
+                echo '<div class="alert">Deze pagina bestaat niet!</div>';
+                // echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+                header( "refresh:4;url=http://www.pro-magazijn.com/CWControllers" );
+                // die;
+                // ob_flush();
+                
+            }          
+        }
+        public function index()
+        {            
             $data = [
-                'title' => 'Warehouses'
+                'title' => 'Warehouses',
+                'warehouses' => $this->getAll()
             ];
-            $this->view('ChooseW/index', $data);
+            $this->view('warehouse/index', $data);
         }
     }
