f09e369141907ebbae911ca5582637b2fd43f60c
MSI\roywi <roywilliamvermeulen@gmail.com>
2022-10-09 13:50:18 +0200

    Made to student dashboard itteration1

--- app/views/dashboards/student.php
+++ app/views/dashboards/student.php
@@ -16,7 +16,26 @@
             </div>
             <div class="main-grid-items">
                 <div class="container-3">
+                    <div>
+                        <div class="unigrid-col2">
+                            <h1 class="GridHeader1">Items geleend:</h1>
+                            <h1 class="GridHeader2">Actief:</h1>
 
+                            <div class="GridCol1">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis at, rerum alias velit enim asperiores qui ex non optio vitae excepturi mollitia amet magnam minus perspiciatis est modi veniam voluptas?
+                            </div>
+                            <div class="GridCol2">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo corporis hic reiciendis. Iure fuga nisi cum ipsa est, libero vel obcaecati, nesciunt maiores dolore magnam repudiandae quibusdam beatae porro neque.
+                            </div>
+                        </div>
+                        <div class="unigrid-col2">
+                            <h1 class="GridHeader1">Leen item:</h1>
+                            <h1 class="GridHeader2">Item annuleren:</h1>
+
+                            <div class="GridCol1"><a href=""><button>Lenen</button></a></div>
+                            <div class="GridCol2"><a href=""><button>Annuleer</button></a></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
