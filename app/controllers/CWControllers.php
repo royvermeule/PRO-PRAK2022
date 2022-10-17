aeba50d8cc98e5b326a4cdb3666df9e5e5f87818
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2022-10-17 11:16:08 +0200

    added a check to see if a user is allowed to go in a certain wearhouse

--- app/controllers/CWControllers.php
+++ app/controllers/CWControllers.php
@@ -14,36 +14,52 @@
             $this->cwModel = $this->model('CWModel');
             //var_dump($this->getAll());
         }
-        private function alert(){
-            $this->alert("Dit bestaat niet");
-            $this->view('warehouse/index', );
-            exit;
-        }
+
         public function getAll()
         {
             $this->db->query("SELECT * FROM warehouse");
             return $this->db->resultSet();
         }
-        public function overview($id) {
+
+        public function warehouseAuth($currentWarehouseId) 
+        {   
+
+            // sessie opvragen en data zetten als je inlogt
+            // print_r($_SESSION['user']) ;
+            $userid = $_SESSION["user"]->id;
+            $academyID = $this->db->query("SELECT `academyId` FROM useracademy WHERE `userId` = :id");
+            $this->db->bind(':id', $userid); // 1 veranderen naar je sessie user id
+            $academyID = $this->db->resultSet();
+
+            $sqlToev = "";
+            foreach($academyID as $k => $v){
+                $sqlToev .= empty($sqlToev) ? "academyId = '".$v->academyId."'" : " or academyId = '".$v->academyId."'";
+            }
+
+            $warehouseId = $this->db->query("SELECT `warehouseId` FROM `warehouseacademy` WHERE $sqlToev");
+            $warehouseId = $this->db->resultSet();
             
-            if($id == 1 ){
-                $this->view('warehouse/1');
-                        }
-            elseif($id == 2){
-                $this->view('warehouse/2');
+            $mapping = array();
+
+            foreach($warehouseId as $k => $v) {
+                array_push($mapping, $v->warehouseId);
             }
-            elseif($id == 3){
-                $this->view('warehouse/3');
+
+            if(in_array($currentWarehouseId, $mapping)){
+                $this->view('warehouse/' . $currentWarehouseId);
+            }else {
+                return true;
+                header("Location: /CWControllers/");
             }
-            else{
-                //ob_start();
-                echo '<div class="alert">Deze pagina bestaat niet!</div>';
-                // echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+        }
+
+        public function generateOverview($id) {
+
+            if($this->warehouseAuth($id)) {
+                echo '<div class="alert">Deze pagina bestaat niet of geen rechten!</div>';
                 header( "refresh:4;url=http://www.pro-magazijn.com/CWControllers" );
-                // die;
-                // ob_flush();
-                
-            }          
+            }
+
         }
         public function index()
         {            
